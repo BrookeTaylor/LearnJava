1ce9eddc779fdd54b642a29572a422d4a743889e
Brooke Taylor <anonyoumiss@gmail.com>
2025-12-02 01:14:47 -0600

    Working on Dog Shelter Manager, more to come!

---
+++ Week_03/DogShelterManager/src/Dog.java
@@ -0,0 +1,23 @@
+public class Dog {
+
+    // fields
+    String name;
+    String breed;
+    String color;
+    int age;
+
+    // constructor
+    public Dog(String name, String breed, String color, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.color = color;
+        this.age = age;
+    }
+
+    // toString Override
+    @Override
+    public String toString() {
+        return "Dog{name='" + name + "', breed='" + breed + "', color='" + color + "', age=" + age + "}";
+    }
+
+}
---
+++ Week_03/DogShelterManager/src/Main.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // shelter list
+        ArrayList<Dog> shelterDogs = new ArrayList<>();
+
+
+        // dogs in shelter
+        shelterDogs.add(new Dog("Mochi", "Corgi", "Tan", 3));
+        shelterDogs.add(new Dog("Bailey", "Golden Retriever", "Golden", 5));
+        shelterDogs.add(new Dog("Shadow", "German Shepherd", "Black/Tan", 7));
+        shelterDogs.add(new Dog("Pepper", "Australian Shepherd", "Merle", 3));
+        shelterDogs.add(new Dog("Maple", "Shiba Inu", "Red", 2));
+        shelterDogs.add(new Dog("Rex", "Pitbull", "Blue", 4));
+        shelterDogs.add(new Dog("Biscuit", "Beagle", "Tri-Color", 6));
+        shelterDogs.add(new Dog("Nova", "Border Collie", "Black/White", 1));
+        shelterDogs.add(new Dog("Tank", "Rottweiler", "Black/Rust", 8));
+        shelterDogs.add(new Dog("Piper", "Dalmatian", "White/Black Spots", 3));
+        shelterDogs.add(new Dog("Hazel", "Pomeranian", "Cream", 4));
+        shelterDogs.add(new Dog("Goose", "Great Dane", "Harlequin", 5));
+        shelterDogs.add(new Dog("Nori", "Akita", "White", 6));
+
+
+
+        // print dogs in shelter
+        printAllDogs(shelterDogs);
+
+
+        // find by name : Goose
+        System.out.println("\nLooking for Goose");
+        Dog found = findDogByName(shelterDogs, "Goose");
+
+        if (found != null) {
+            System.out.println("Found: " + found);
+        } else {
+            System.out.print("Dog not found.");
+        }
+
+
+
+
+        System.out.println("\nFinding the oldest dog...");
+        Dog oldest = findOldestDog(shelterDogs);
+
+        if (oldest != null) {
+            System.out.println("Oldest dog: " + oldest);
+        }
+
+
+    }
+
+
+    // print all dogs
+    static void printAllDogs(ArrayList<Dog> dogs) {
+        for (Dog d : dogs) {
+            System.out.println(d);
+        }
+    }
+
+    // find dog by name
+    static Dog findDogByName(ArrayList<Dog> dogs, String name) {
+        for (Dog d : dogs) {
+            if (d.name.equalsIgnoreCase(name)) {
+                return d;
+            }
+        }
+        return null;
+    }
+
+
+
+    static Dog findOldestDog(ArrayList<Dog> dogs) {
+        if (dogs.isEmpty()) {
+            return null;
+        }
+
+        Dog oldest = dogs.get(0);
+
+        for (Dog d : dogs) {
+            if (d.age > oldest.age) {
+                oldest = d;
+            }
+        }
+
+        return oldest;
+    }
+
+
+
+
+
+
+}
